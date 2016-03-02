8a07d4ae1943d0e8f82900e65baaabec227a54ab
Arthur Caillau <arthur@stuph.co>
2016-03-01 17:33:15 -0800

    Updated description in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject trie "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Trie Datastructure for clj and cljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
